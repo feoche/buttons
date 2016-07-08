84fc2594ecfe0cd7ab07746a72e4536f9721ffc8
FEOCHE <68q759y@>
2016-07-08 18:26:20 +0200

    Trying to add routes

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
-var app = angular.module('app', [])
+var app = angular.module('app', ['ui.router'])
   .controller('AppController', ['$scope', '$http', '$sce', function($scope, $http, $sce) {
     var vm = this;
 
     var COLORS = ['red', 'pink', 'purple', 'deeppurple', 'indigo', 'blue', 'lightblue', 'cyan', 'teal', 'green', 'lightgreen', 'lime', 'yellow', 'amber', 'orange', 'deeporange', 'brown', 'grey', 'bluegrey', 'black', 'white'],
       colors = COLORS;
 
-    vm.buttons = [];
+    $scope.buttons = [];
     $http.get('json/data.json').then(function(data){
-      vm.buttons = data.data;
-      angular.forEach(vm.buttons, function(i){
+      $scope.buttons = data.data;
+      angular.forEach($scope.buttons, function(i){
         var rand = Math.floor(Math.random()*colors.length);
         i.class = colors[rand] + '-button';
         var tmp = [];
@@ -23,19 +23,39 @@ var app = angular.module('app', [])
         else {
           colors = COLORS;
         }
-        i.src = $sce.trustAsResourceUrl('sounds/' + i.src + '.mp3');
+        i.source = $sce.trustAsResourceUrl('sounds/' + i.src + '.mp3');
       });
     });
 
-    vm.play = function(title) {
-      for (var i = 0; i < vm.buttons.length; i++) {
-        var item = document.getElementsByClassName("audio-" + vm.buttons[i].title);
-        if(vm.buttons[i] && item && item[0] && vm.buttons[i].title !== title) {
+    $scope.play = function(title) {
+      for (var i = 0; i < $scope.buttons.length; i++) {
+        var item = document.getElementsByClassName('audio-' + $scope.buttons[i].title);
+        if($scope.buttons[i] && item && item[0] && $scope.buttons[i].title !== title) {
           item[0].pause();
         }
       }
-      var audio = document.getElementsByClassName("audio-" + title)[0];
+      var audio = document.getElementsByClassName('audio-' + title)[0];
       audio.currentTime = 0;
       audio.play();
     };
-  }]);
+  }])
+  .config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('buttons', {
+        url: '/',
+        templateUrl: 'views/all.html',
+        controller: 'AppController as vm'
+      })
+      .state('item', {
+        url: '/{src}',
+        templateUrl: 'views/button.html',
+        controller: function($scope, $stateParams){
+          angular.forEach($scope.buttons, function(item) {
+            if(item.src === $stateParams.src) {
+              $scope.button = item;
+            }
+          });
+        }
+      });
+    $urlRouterProvider.otherwise('/');
+  });
