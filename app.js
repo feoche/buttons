0b89335293499647a107f7b205307a14a85f466e
François Eoche <francois.eoche@corp.ovh.com>
2021-12-21 08:58:45 +0100

    up

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
-var express = require('express');
+const express = require('express');
 const fs = require('fs');
 
-var app = express();
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    const result = fs.readFileSync('./json/data.json');
+app.get('/sounds', (req, res) => {
+    let result = JSON.parse(fs.readFileSync('./json/data.json'));
     res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.parse(result));
+    const query = req.query.query;
+    if(query) {
+        result = result.filter((item) => {
+            const { title, description, keywords } = item;
+            let filter = (title && title.toLowerCase().includes(query.toLowerCase()))
+                    || (description && description.toLowerCase().includes(query.toLowerCase()))
+                    || (keywords && keywords.some((keyword) => keyword.includes(query.toLowerCase())));
+            return filter;
+        });
+    }
+    res.send(result);
 });
 
-app.use('/sounds', express.static('sounds'));
+app.get('/sounds/:id', (req, res) => {
+    let result;
+    try {
+        result = fs.readFileSync(`./sounds/${req.params.id}.mp3`);
+    } catch (e) {
+        result = fs.readFileSync('./sounds/coin.mp3');
+    }
+    res.setHeader('Content-Type', 'audio/mpeg');
+    res.send(result);
+});
 
-app.listen(8000, () => console.info("serveur lancé => http://localhost:8000"));
+let server = app.listen(process.env.PORT || 80, () => console.info("serveur lancé => http://localhost"));
+setInterval(() => {
+    server.close();
+    server = app.listen(process.env.PORT || 80, () => console.info("serveur lancé => http://localhost"));
+}, 600000);
